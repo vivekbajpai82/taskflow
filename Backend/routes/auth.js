88bddae84cb91aa663a8bf42369c00697f9bf206
vivekbajpai82 <bajpaivivek82@gmail.com>
2026-04-30 15:54:10 +0530

    fix: user model import

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-require('../models/user')
+const User = require('../models/user');
 const { protect } = require('../middleware/auth');
 
 const router = express.Router();
